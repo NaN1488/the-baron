d2069f2f9a34b20db175605cacb488558a01db6f
Nicolas Rodas <nicolas.rodas@kinnser.com>
2012-11-06 15:04:23 -0300

    Front end refactor, added folders for templates (work in progress)

---
+++ client/templates/js/playlist.js
@@ -0,0 +1,37 @@
+Template.playlist.videos = function() {
+	return Videos.find({},{ sort: {hour: -1} });
+}
+
+Template.playlist.events({	
+	'click input#play': function() {
+    var key = $('#url').val();
+    playVideo(key);
+	}
+});
+
+  Template.playlist.events({
+    'click input#emptyPlaylist': function() {
+      var reply = prompt("Hey you! You are about to remove ALL Videos, give us the passphrase or you will be EXTERMINATED!", "");
+      //TODO: check user is admin
+      if(reply == "tategay") 
+        Videos.remove({});
+    }
+  });
+
+  Template.playlist.events({
+    'click input.deleteSong': function() {
+      var reply = prompt("Hey you! You are about to remove a video, give us the passphrase or DIE!", "");
+      //TODO: check user is admin
+      if(reply == "tategay") 
+        Videos.remove({ key: (this).key });
+    }
+  });
+
+   Template.playlist.events({
+    'click input.replaySong': function() {
+      var reply = prompt("Hey you! You are about to make us all hear the same song again, it must be GOOD! So give us the passphrase or run!", "");
+      //TODO: check user is admin
+      if(reply == "tategay") 
+        playVideo((this).key);
+    }
+  });
