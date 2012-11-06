c8bb1fd23ea87ee75bf491b47c43e73b9c2ae6ee
Nicolas Rodas <nicolas.rodas@kinnser.com>
2012-11-06 15:08:06 -0300

    Removed playlist template from client.js, css refactor

--- client/client.js
+++ client/client.js
@@ -39,10 +39,6 @@ Handlebars.registerHelper('user_logged_in', function() {
      return (Meteor.user() !== null);
 });
 
-Template.playlist.videos = function() {
-	return Videos.find({},{ sort: {hour: -1} });
-}
-
 //return video_id
 Template.player.current_video = function() {
 	return Controller.current_video();	
@@ -86,13 +82,6 @@ function playVideo(key) {
     });
   }
 }
-
-Template.playlist.events({	
-	'click input#play': function() {
-    var key = $('#url').val();
-    playVideo(key);
-	}
-});
 
 /**
 * Adding the Chat JS
@@ -180,24 +169,6 @@ Template.entry.events[okcancel_events('#messageBox')] = make_okcancel_handler({
     }
   });
 
-  Template.playlist.events({
-    'click input#emptyPlaylist': function() {
-      var reply = prompt("Hey you! You are about to remove ALL Videos, give us the passphrase or you will be EXTERMINATED!", "");
-      //TODO: check user is admin
-      if(reply == "tategay") 
-        Videos.remove({});
-    }
-  });
-
-  Template.playlist.events({
-    'click input.deleteSong': function() {
-      var reply = prompt("Hey you! You are about to remove a video, give us the passphrase or DIE!", "");
-      //TODO: check user is admin
-      if(reply == "tategay") 
-        Videos.remove({ key: (this).key });
-    }
-  });
-
   Template.message.events({
     'click input.deleteChatLine': function() {
       var reply = prompt("Hey you! You are about to remove a chat line, give us the passphrase or your line will be there forever!", "");
@@ -221,14 +192,6 @@ Template.entry.events[okcancel_events('#messageBox')] = make_okcancel_handler({
     }
   });
 
-  Template.playlist.events({
-    'click input.replaySong': function() {
-      var reply = prompt("Hey you! You are about to make us all hear the same song again, it must be GOOD! So give us the passphrase or run!", "");
-      //TODO: check user is admin
-      if(reply == "tategay") 
-        playVideo((this).key);
-    }
-  });
 
   Template.messages.messages = function(){
     return Messages.find({}, { sort: {time: -1} });
