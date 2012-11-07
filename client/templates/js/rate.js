c2a67fbcd835f4fd6aa30659ba28d4445b73d5fd
seba <sebastian.scandolo@veloctiypartners.net>
2012-11-07 10:27:00 -0300

    The rate is back baby

--- client/templates/js/rate.js
+++ client/templates/js/rate.js
@@ -1,21 +1,21 @@
-  Template.rateas.rate = function(){
-    current_video = CurrentVideos.findOne({channel: 'default'});
-    if (current_video != undefined) {
-       current_video_id = current_video.video_id;
-       cursor = Rates.find({video: current_video_id});
-       var avg = 0, total = 0;
-       count = cursor.count();
-       if(count == 0) {
-          avg = 72; //VP rule
-       } else {
+   Template.rateas.rate = function(){
+      current_video = CurrentVideos.findOne({channel: 'default'});
+      if (current_video != undefined) {
+         current_video_id = current_video.video_id;
+         cursor = Rates.find({video: current_video_id});
+         var avg = 0, total = 0;
+         count = cursor.count();
+         if(count == 0) {
+           avg = 72; //VP rule
+         } else {
           cursor.forEach(function(e)
-        {
-          total += e.rate;
-        });
-        avg = Math.round((total / count) * 25);
-        return avg.toString();
-       }
-    }
+         {
+           total += e.rate;
+         });
+         avg = Math.round((total / count) * 25);
+         return avg.toString();
+        }
+     }
     return "72"
   }
 
