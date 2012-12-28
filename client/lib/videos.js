a9312086c4730ce0d020b8cbf59f8dade626d748
Nan <nahuelsciaratta@gmail.com>
2012-12-28 15:43:10 -0300

    never used, remove

--- client/lib/videos.js
+++
@@ -1,8 +0,0 @@
-
-VideosWrapper = {
-	current: function(channel){
-		if (channel == undefined) channel = 'default'
-		current_videos = CurrentVideos.find({channel:channel}).fetch()[0];
-		return Videos.find({key:current_videos.video_id}).fetch()[0]
-	}
-}
