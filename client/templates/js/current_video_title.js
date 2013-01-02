ea89fde67bdc3c0f8900f456dd3bd40b8fd83e93
Nan <nahuelsciaratta@gmail.com>
2013-01-02 17:23:57 -0300

    refactoring and clean up

--- client/templates/js/current_video_title.js
+++ client/templates/js/current_video_title.js
@@ -1,10 +1,10 @@
 Template.current_video_title.title = function (){
 	channel = Channels.find({name:'default'}).fetch();
-	if (channel.length > 0){
-		console.log(channel);
+	if (channel.length > 0) {
 		video_id = channel[0].video_id
-		return Videos.find({key: video_id}).fetch()[0].title
+		videos = Videos.find({key: video_id}).fetch();
+		if (videos.length >0) return videos[0].title
 	}
-	return 'untitle';
+	return '';
 	
 }
