89d0bfb02eed1543aa1297d5eeaa5093f60a7c01
Nan <nahuelsciaratta@gmail.com>
2013-01-03 18:54:35 -0300

    add template for results

--- client/lib/video_results.js
+++ client/lib/video_results.js
@@ -1,7 +1,13 @@
 VideoResults = {
 	_:{
 		result_container:'#list_videos',
-		videos_result: null
+		videos_result: null,
+		template_result:'<div class="video_result span2" data-video="%video_id%">'+
+			'<div class="img"><span class="duration">%duration%</span><img src="%img_url%"/></div>'+
+			'<div class="result_title">%title%</div>'+
+			//'<div class="result_description movie-synopsis">%description%</div>'+
+			''+
+			'</div>'
 	},
 	fill_result_list: function (videos, source){
 		var html = '';
@@ -20,21 +26,13 @@ VideoResults = {
 	    time.setSeconds(entry.media$group.yt$duration.seconds);
 	    var minutes = time.toTimeString().substring(3,9);
 	    video_id = this.get_video_id(entry);
-	    var markup = "<table class='movie-result' ><tr>";
-	        //if (entry.media$group.media$thumbnail !== undefined && movie.media$group.media$thumbnail[0] !== undefined) {
-	        markup += "<td class='movie-image'><img data-video-id='"+video_id+"' class='youtube-search-result' src='" + entry.media$group.media$thumbnail[3].url + "' /><strong class='movie-synopsis'>" + minutes + "</strong></td>";
-	       
-	        //}
-	        markup += "<td class='movie-info'><div class='movie-title'>" + entry.title.$t + "</div>";
-	        
-	        
-	        
-	        if (entry.content.type == 'text') {
-	            markup += "<div class='movie-synopsis'>" + entry.content.$t.substring(0, 200); + "</div>";
-	        }
-
-	        markup += "</td></tr></table>";
-	        return "<div class='video_result' data-video='"+video_id+"'>" + markup +'</div>';
+	    var template = this._.template_result;
+	    template = template.replace('%video_id%',video_id);
+	    template = template.replace('%title%', entry.title.$t.substring(0, 55));
+	    template = template.replace('%img_url%', entry.media$group.media$thumbnail[3].url);
+	    template = template.replace('%description%', entry.content.$t.substring(0, 70));
+	    template = template.replace('%duration%', minutes);
+	    return template;
 	},
 	video: function (id){
 		var video = null;
