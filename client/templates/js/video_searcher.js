8be2172676dcc229266061f4fa58aabff7bdcacf
Nan <nahuelsciaratta@gmail.com>
2013-01-03 13:05:04 -0300

    youtube searcher with autocomplete

--- client/templates/js/video_searcher.js
+++ client/templates/js/video_searcher.js
@@ -1,3 +1,7 @@
+Template.video_searcher.rendered =  function (){
+	YoutubeAutocomplete.bind_autocomplete();
+}
+
 Template.video_searcher.events({
 	'keypress input#video_searcher': function (e){
 		if (e.keyCode != 13 ) return; //if not "enter" key
@@ -5,6 +9,7 @@ Template.video_searcher.events({
 	},
 	'click #btn_video_searcher': function (e){
 		VideoSearcher.youtube($('input#video_searcher').val());
+		e.preventDefault();
 	},
 	'click .video_result': function (e){
 		playVideo($(e.currentTarget).data('video'));
