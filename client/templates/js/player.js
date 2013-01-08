1097f1813c38851441f725ebee9aca9f6be3d114
Nan <nahuelsciaratta@gmail.com>
2013-01-08 12:18:52 -0300

    remove function template that is not required

--- client/templates/js/player.js
+++ client/templates/js/player.js
@@ -1,14 +1,7 @@
-
 //return video_id
 Template.player.current_video = function() {
   return Controller.current_video();	
 }
-
-//this function is called from the template in order to load the video for any client
-Template.player.load_video = function (){
-  Controller.load_video();
-}
-
  //load_viewer will create _player once
 Template.player.load_viewer = function (){
   if (_player == undefined) {
