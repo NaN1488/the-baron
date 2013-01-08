e14203239414ac9b866757e6dd609e5699c70181
Bruno Bonamin <bruno@bonamin.org>
2013-01-08 11:36:03 -0300

    Pending switching channels

--- client/lib/Controller.js
+++ client/lib/Controller.js
@@ -7,9 +7,9 @@ Controller = {
 	//this method propagate the selected video to the rest of the clients
 	load_video: function (channel){
 		console.log('load_video');
-		if (channel === undefined) channel = this._.default_channel;
+		var channel = ChannelHelper.current();
 		//check if _player is ready
-		if (_player != undefined && _player.loadVideoById != undefined){
+		if (_player != undefined && _player.loadVideoById != undefined && channel != undefined){
 
 	 		Meteor.call('current_time_video', function (err, time){
 	 			var current_video_id = Controller.current_video();
@@ -24,17 +24,16 @@ Controller = {
  		}
 	},
 	// return video id for a given channel
-	current_video: function (channel){
-		if (channel === undefined) channel = this._.default_channel;
-		channels = Channels.find({name:channel}).fetch();
-		if(channels.length == 0) return false;
-		return channels[0].video_id;
+	current_video: function (){
+		var channel = ChannelHelper.current();
+		if(channel == undefined) return false;
+		return channel.video_id;
 	},
 	change_video: function (video_id, channel){
 		console.log('change_video')
-		if (channel === undefined) channel = this._.default_channel;
+		var channel = ChannelHelper.current();
 		Meteor.call('get_server_time', function (err, time){	
-	 		Channels.update({name:channel}, 
+	 		Channels.update({_id: channel._id}, 
 	 			{
 	 				$set:{
 	 					video_id: video_id,
--- server/methods.js
+++ server/methods.js
@@ -2,12 +2,12 @@
 expose methods to call from the clients
 **/
 BaronServer = {
-	current_time_video: function (channel){
-		if (channel === undefined) channel = 'default'
-		current_channel = Channels.find({name:channel}).fetch()[0];
-		//TODO: get the duration of the current video
-		diff_time = (Date.now() - current_channel.start_at) / 1000;
-		return Math.floor(diff_time);
+	current_time_video: function (current_channel){
+    if(current_channel != undefined){
+      //TODO: get the duration of the current video
+      diff_time = (Date.now() - current_channel.start_at) / 1000;
+      return Math.floor(diff_time) ;
+    }
 	},
 	get_server_time: function (){
 		return Date.now();
