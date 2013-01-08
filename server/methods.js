f56bcf50fe4c44b30a2794ddcb278ce0ac93b04d
Nan <nahuelsciaratta@gmail.com>
2013-01-08 10:37:36 -0300

    move add_video_to_queue into the server if channel doesn't have any current video, play the new one on the queue

--- server/methods.js
+++ server/methods.js
@@ -3,16 +3,35 @@ expose methods to call from the clients
 **/
 BaronServer = {
 	current_time_video: function (channel){
-		if (channel === undefined) channel = 'default'
+		if (channel === undefined) channel = 'default';
 		current_channel = Channels.find({name:channel}).fetch()[0];
-		duration = Videos.find({key:current_channel.video_id}).fetch()[0].duration;
-		diff_time = (Date.now() - current_channel.start_at) / 1000;
-		if (diff_time < duration)
-			return Math.floor(diff_time);
+		if (current_channel.video_id != ''){
+			duration = Videos.find({key:current_channel.video_id}).fetch()[0].duration;
+			diff_time = (Date.now() - current_channel.start_at) / 1000;
+			console.log(diff_time, duration);
+			if (diff_time < (duration+5)){
+				return {time:Math.floor(diff_time), video_id: current_channel.video_id};
+			} else {
+				VideoTrigger.play_next_in_queue(channel);
+			}
+		}else{
+			VideoTrigger.play_next_in_queue(channel);
+		}
 		return null;
 	},
 	get_server_time: function (){
 		return Date.now();
+	},
+	add_video_to_queue: function(video_id, channel){
+		console.log('add_video_to_queue', video_id);
+		if (channel === undefined) channel = 'default';
+		channel_data = Channels.findOne({name:channel});
+		videos = channel_data.videos_in_queue;
+		videos.push(video_id);
+		Channels.update({name:channel}, {$set: {videos_in_queue: videos}});
+		if (channel_data.video_id == ''){
+			VideoTrigger.play_next_in_queue(channel);
+		}
 	}
 }
 /**
