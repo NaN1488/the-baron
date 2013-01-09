dc81b64b1faccc624c98447af47797846236daf2
Bruno Bonamin <bruno@bonamin.org>
2013-01-09 10:26:58 -0300

    Channel form improvements

--- client/templates/js/channels_list.js
+++ client/templates/js/channels_list.js
@@ -7,13 +7,20 @@ Template.channels_list.events({
     $('#new_channel form').toggle();
   },
   'submit form': function(e){
-    console.log('form submitted!');
-    $target = $(e.target);
     e.preventDefault();
+    $target = $(e.target);
+    
+    $('#error').remove();
+    $name = $target.find('[name=name]')
+
     var userId = Meteor.userId;
-    var name = $target.find('[name=name]').val();
+    var name = $name.val();
+    
     if(Channels.findOne({name: name}) == undefined){
       Channels.insert({name: name, video_id:'', start_at: 0, user_id: userId});
+      $name.val('');
+    } else {
+      $('#new_channel form').append('<p id="error">Name has been taken</p>');
     }
   }
 })
