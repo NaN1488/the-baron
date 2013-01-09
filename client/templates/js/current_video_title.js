83905854306510b7eeb01a2a67ed89be97c4b994
Bruno Bonamin <bruno@bonamin.org>
2013-01-09 11:47:07 -0300

    change video title according to channel

--- client/templates/js/current_video_title.js
+++ client/templates/js/current_video_title.js
@@ -1,9 +1,9 @@
 Template.current_video_title.title = function (){
-	channel = Channels.find({name:'default'}).fetch();
-	if (channel.length > 0) {
-		video_id = channel[0].video_id
-		videos = Videos.find({key: video_id}).fetch();
-		if (videos.length >0) return videos[0].title
+	channel = ChannelHelper.current();
+	if (channel != undefined) {
+		video_id = channel.video_id
+		video = Videos.findOne({key: video_id});
+		if (video != undefined) return video.title;
 	}
 	return '';
 	
